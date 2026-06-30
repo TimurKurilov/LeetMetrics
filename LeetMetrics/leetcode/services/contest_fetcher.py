fc3f77b93c69727a356e13ad8ed94135ab645b30
TimurKurilov <kyrilov.timur@gmail.com>
2026-06-30 18:50:16 +0300

    selinium

--- LeetMetrics/leetcode/services/contest_fetcher.py
+++ LeetMetrics/leetcode/services/contest_fetcher.py
@@ -1,20 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import requests
 from leetcode.graphql_queries.contest_queries import USER_CONTEST_QUERY
 
+
 class LeetCodeContestFetcher:
+
+    def _get_cookies(self, username):
+        options = Options()
+        options.add_argument("--start-maximized")
+        driver = webdriver.Chrome(options=options)
+        driver.get(f"https://leetcode.com/{username}/")
+        driver.implicitly_wait(10)
+        cookies = driver.get_cookies()
+        driver.quit()
+
+        return cookies
+
+    def _build_session(self, cookies):
+        session = requests.Session()
+
+        for cookie in cookies:
+            session.cookies.set(cookie['name'], cookie['value'])
+
+        session.headers.update({
+            "User-Agent": "Mozilla/5.0",
+            "Referer": "https://leetcode.com",
+            "Content-Type": "application/json"
+        })
+
+        return session
+
     def fetch_contest(self, username):
-        self.url = "https://leetcode.com/graphql"
-        variables = {
-            "username": username
-        }
-        
-        response = requests.post(
-            self.url,
+        url = "https://leetcode.com/graphql"
+
+        cookies = self._get_cookies(username)
+        session = self._build_session(cookies)
+
+        variables = {"username": username}
+
+        response = session.post(
+            url,
             json={
                 "query": USER_CONTEST_QUERY,
                 "variables": variables
             }
         )
-        
-        data = response.json()
-        return data
+
+        return response.json()
